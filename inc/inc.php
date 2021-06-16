f8993ab88c3636c4da0d57dcede4aa2ace2934cf
Romian Tairovski <romian.tairovski@gmail.com>
2021-06-16 13:06:30 +0200

    Friends

--- inc/inc.php
+++ inc/inc.php
@@ -1,6 +1,8 @@
+<link rel="stylesheet" href="style/style.css">
+
 <?php
 error_reporting(E_ALL);
 ini_set('display_errors', 'On');
 include_once('class/DB.class.php');
 
-$db = new db('172.28.64.1', 'dg3', 'password', 'dg3');
+$db = new db('10.115.255.52', 'dg3', 'password', 'dg3');
--- profile.php
+++ profile.php
@@ -2,10 +2,32 @@
 include_once('inc/inc.php');
 $account = $db->query('SELECT * FROM users WHERE id = ?', $_GET['id'])->fetchArray();
 
-$friends = $db->query('SELECT * FROM friends WHERE user = ?', $account['id'])->numRows();
-$followers = $db->query('SELECT * FROM friends WHERE friend = ?', $account['id'])->numRows();
+$friendsAmount = $db->query('SELECT * FROM friends WHERE user = ?', $account['id'])->numRows();
+$friends = $db->query('SELECT users.*, friends.`user`, friends.friend FROM users , friends WHERE friends.`user` = ? AND friends.friend = users.id ', $account['id'])->fetchAll();
+
+$followersAmount = $db->query('SELECT * FROM friends WHERE friend = ?', $account['id'])->numRows();
+$followers = $db->query('SELECT users.id, users.username, users.profilepicture, users.`password`, users.background, users.font, users.lastLogin, users.timeCreated FROM friends , users WHERE friends.friend = ? AND friends.user = users.id', $account['id'])->fetchAll();
 ?>
 <h1><?=$account['username']?></h1>
 
-<p><?=$friends?> friends</p>
-<p><?=$followers?> followers</p>
+<p><?=$friendsAmount?> friends</p>
+<p><?=$followersAmount?> followers</p>
+<p>Your friends</p>
+<table>
+    <?php foreach($friends as $friend) { ?>
+    <tr>
+        <td><?=$friend['id']?></td>
+        <td><a href="profile?id=<?=$friend['id']?>"><?=$friend['username']?></a></td>
+    </tr>
+    <?php } ?>
+</table>
+
+<p>Your followers</p>
+<table>
+    <?php foreach($followers as $follower) { ?>
+    <tr>
+        <td><?=$follower['id']?></td>
+        <td><a href="profile?id=<?=$follower['id']?>"><?=$follower['username']?></a></td>
+    </tr>
+    <?php } ?>
+</table>
